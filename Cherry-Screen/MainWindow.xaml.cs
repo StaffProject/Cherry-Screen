dee5561d079bc52a0e9417a2f636d8b1ca9812be
StaffProject <kaszanka@o2.pl>
2015-01-20 17:28:28 +0100

    grafika do duzych przyciskow narazie bez ikon

--- Cherry-Screen/MainWindow.xaml.cs
+++ Cherry-Screen/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Cherry_Screen
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
